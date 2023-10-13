38f77a3fb460b25ade90999e0d6f345238fa9cea
benjamin <benjaminrodas934@gmail.com>
2023-10-13 14:58:33 +0800

    make playRound function of rock paper scissor

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,26 @@
+function playRound(playerSelection, computerSelection){
 
+    if (
+        (playerSelection === computerSelection)
+        ){
+            return "It's a tie!"
+        } else if (
+            (playerSelection.toLowerCase() === "Rock" &&
+             computerSelection.toLowerCase() === "Scissor")||
+            
+            (playerSelection.toLowerCase() === "Paper" &&
+             computerSelection.toLowerCase() === "Rock") ||
 
-
-
-
-
+            (playerSelection.toLowerCase() === "Scissor" &&
+             computerSelection.toLowerCase() === "Paper")
+        ) {
+            return `You win! ${playerSelection} beats 
+            ${computerSelection}.`
+        } else {
+            return `You lose! ${computerSelection} beats 
+            ${playerSelection}.`
+        }
+}
 
 
 
