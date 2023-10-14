6915ad63209beaa75b09100b39817413c5a7d664
benjamin <benjaminrodas934@gmail.com>
2023-10-14 14:47:29 +0800

    add eventListener to function game and make player/computer Score result

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,7 @@
 function playRound(playerSelection, computerSelection){
 
     if (
-        (playerSelection === computerSelection)
+        (playerSelection.toLowerCase() === computerSelection.toLowerCase())
         ){
             return "It's a tie!"
         } else if (
@@ -14,28 +14,33 @@ function playRound(playerSelection, computerSelection){
             (playerSelection.toLowerCase() === "Scissor" &&
              computerSelection.toLowerCase() === "Paper")
         ) {
-            return `You win! ${playerSelection} beats 
-            ${computerSelection}.`
+            return `You win! ${playerSelection} beats ${computerSelection}.`
         } else {
-            return `You lose! ${computerSelection} beats 
-            ${playerSelection}.`
+            return `You lose! ${computerSelection} beats ${playerSelection}.`
         }
 }
 
-function game() {
+document.addEventListener("click",function game() {
 
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++);
-    
-    const playerSelection = prompt("pick one Rock, Paper, Scissor")
-    const computerSelection = 
-    ['Rock', 'Paper', 'Scissor']
-    [ Math.floor(Math.random() * 3)];
-   
+    for (let i = 0; i < 5; i++){
+        const playerSelection = prompt("pick one Rock, Paper, Scissor")
+        const computerSelection = 
+        ['Rock', 'Paper', 'Scissor']
+        [ Math.floor(Math.random() * 3)];
+       
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        if (result.includes("win")){
+            playerScore++
+        } else if(result.includes("lose")){
+            computerScore++
+        }
 
-   
-}
+
+    }
+})
 
 
 
