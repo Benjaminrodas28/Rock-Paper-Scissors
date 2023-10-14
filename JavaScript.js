5b57c4f936b711c0ad01d9d9d136a0c68522f12c
benjamin <benjaminrodas934@gmail.com>
2023-10-14 11:00:20 +0800

    function game playerScore and coputerScore

--- JavaScript.js
+++ JavaScript.js
@@ -23,50 +23,19 @@ function playRound(playerSelection, computerSelection){
 }
 
 function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++);
     
     const playerSelection = prompt("pick one Rock, Paper, Scissor")
     const computerSelection = 
-    ["Rock", "Paper", "Scissor"]
+    ['Rock', 'Paper', 'Scissor']
     [ Math.floor(Math.random() * 3)];
+   
 
-
-
+   
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
 
